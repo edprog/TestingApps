550a5a5ccf57d26d4a89514977478ebbd9c8a34b
Edwin Rodriguez <edwin_r81@yahoo.com>
2015-07-26 01:08:55 -0400

    Initial commit

---
+++ RepZioTest/RepZioTest/scripts/script.js
@@ -0,0 +1,28 @@
+﻿var repZioTest = angular.module('repZioTest', ['ngRoute', 'wu.masonry']);
+
+repZioTest.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'pages/products.html',
+            controller: 'mainController'
+        })
+         .when('/products', {
+                    templateUrl: 'pages/products.html',
+                    controller: 'mainController'
+         })
+        .when('/contact', {
+            templateUrl: 'pages/contact.html',
+            controller: 'contactController'
+        });
+});
+
+repZioTest.controller('mainController', ['$scope', '$http',
+  function ($scope, $http) {
+      $http.get('data/data.json').success(function (data) {
+
+          if (($scope.products == null || $scope.products.length == 0) && data != null) {
+              $scope.products = data;
+              $scope.ManufacturerID = data.ManufacturerID;
+          }
+      });
+  }]);
